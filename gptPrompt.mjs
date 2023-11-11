737db83e0c4eb58a7dc8a9a6b1c11b37cd4e83e7
Fahad Faruqi <fahadfaruqi1@gmail.com>
2023-11-11 06:49:13 -0500

    finished v1 of project - needs to be able to query up posts, handle images, and upload SEO information

--- gptPrompt.mjs
+++ gptPrompt.mjs
@@ -13,7 +13,7 @@ function generateChatResponse(apiKey, prompt) {
     openai.chat.completions.create({
       model: 'gpt-4',
       messages: [
-        { role: 'system', content: "You will recieve news articles describing a certain topic or event. You will output json in the following order: an appropiate headline with the prefix (WATCH) if the article describes a video or (READ) if it's just a regular article. The the next items in the json will be the body of the article that was passed - but formatted neatly for block-editor in wordpress. It will also contian tags relevant to the post that are seperated by commas. It will also contain a field for a short SEO title for the post, and lastly a field for a 200-char summary of the article."},
+        { role: 'system', content: "You will recieve news articles describing a certain topic or event. Ignore all text that discusses the text being an email. You will output json in the following order: an appropiate headline (called 'title') with the prefix (WATCH) if the article describes a video or (READ) if it's just a regular article, (PICS) if its a gallery of images, and (BUY) if it's an advertisement. The the next items in the json will be the body (called 'content') of the article that was passed - but formatted neatly for block-editor in wordpress as well as avoiding bad characters that would mess up input into a json object and sending via api (play it safe). It will also contain an array of tags called 'tags' relevant to the post. An array of categories (called 'categories') that the article fits into the following categories gets turned into the respective number it points to (EX: if the text fits BOOKS, convert it to 67): [GAMES-351,MUSIC & VIDEOS-3, BOOKS-67, BREAKING NEWS-12, CULTURE & EVENTS-8, FILM & TV-9, HEALTH & FITNESS-80, PHOTO GALLERY-2, STYLE & BEAUTY-1819, Gift Guide-1819, Review-4894]. A post can have more than one category. Lastly a field for a 200-char summary (called 'excerpt')of the article."},
         { role: 'user', content: prompt }
       ],
     })
@@ -26,8 +26,8 @@ export async function createPost(input) {
     console.log(chalk.blue.bgYellow.bold('Prompting GPT'));
     const response = await generateChatResponse(apiKey, String(input));
     console.log(chalk.white.bgGreen("Success!"));
-    
-    return response;
+
+    return response.choices[0].message.content;
   } catch (error) {
     console.error('Error:', error);
   }
