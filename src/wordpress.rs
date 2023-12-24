94aaaca3c1ea1e404fb4a95d0cd0746cd25e631a
Fahad Faruqi <fahadfaruqi1@gmail.com>
2023-12-24 11:52:00 -0500

    create tag now works!

--- src/wordpress.rs
+++ src/wordpress.rs
@@ -6,24 +6,24 @@ use base64;
 use urlencoding::encode;
 use base64::{Engine as _, engine::general_purpose};
 
-#[tokio::main]
-async fn create_tag(tag: String, url: String, creds: String) -> Result<i64, Box<dyn Error>> {
+async fn create_tag(wordpress: &reqwest::Client, url: &str, creds: &str, tag: &str,) -> Result<i64, Box<dyn std::error::Error>> {
     let body = format!("{{\"name\":\"{}\"}}", tag);
-
-    let wordpress: reqwest::Client = reqwest::Client::new();
     let create_tag_response: reqwest::Response = wordpress.post(url)
-        .header("Authorization", format!("Basic {}", &creds))
+        .header("Authorization", format!("Basic {}", creds))
+        .header("Content-Type", "application/json")
         .body(body)
         .send()
         .await?;
 
     let create_tag_response: String = create_tag_response.text().await?;
-    let create_tag_response: serde_json::Value = serde_json::from_str(&create_tag_response)?;
-    match create_tag_response[0]["id"].as_i64() {
+    let create_tag_response_json: serde_json::Value = serde_json::from_str(&create_tag_response)?;
+
+    match create_tag_response_json["id"].as_i64() {
         Some(id) => Ok(id),
-        None => Err("Something went wrong".into()),
+        None => Err(create_tag_response.to_string().into()),
     }
 }
+
 #[tokio::main]
 async fn find_tag(tag: String) -> Result<i64, Box<dyn Error>> {
     dotenv().expect("Failed to read .env file");
@@ -40,21 +40,23 @@ async fn find_tag(tag: String) -> Result<i64, Box<dyn Error>> {
         .await?;
     let existing_tag: String = existing_tag_response.text().await?;
     let existing_tags: serde_json::Value = serde_json::from_str(&existing_tag)?;
-    print!("{}\n", existing_tags[0]);
 
-    if existing_tags=="[]" { // we must create a new tag
-        Ok(create_tag(tag, url, creds)?)
+    if existing_tags.to_string() =="[]" { // we must create a new tag
+        let result = create_tag(&wordpress, &url, &creds, &tag).await;
+        match result {
+            Ok(result) => Ok(result),
+            Err(err) => Err(err),
+        }
     } else{ // return tag's ID
         let first_tag_id = existing_tags[0]["id"].as_i64().unwrap();
         Ok(first_tag_id)
     }
 }
 
-pub fn wordpress(){
-    let result = find_tag("gambino".to_string());
+pub fn wordpress() {
+    let result = find_tag("testingfahad12345".to_string());
     match result {
         Ok(result) => println!("{}", result),
         Err(err) => println!("Error: {}", err),
     }
-    // println!("{}", "Called wordpress function".green());
 }
