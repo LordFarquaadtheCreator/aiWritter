b98671dc1f2ff2c749ec4c31df99ee212d8e0bdb
Fahad Faruqi <fahadfaruqi1@gmail.com>
2023-12-27 13:04:42 -0500

    Add GPTPrompt struct and save GPT response to JSON file and GPT struct.

--- src/gpt_prompt.rs
+++ src/gpt_prompt.rs
@@ -7,23 +7,32 @@ use std::fs;
 use std::io::{self};
 use dotenv::dotenv;
 use colored::*;
+use std::fs::File;
+use std::io::prelude::*;
 
-/// reads & returns content of files
+pub struct GPTPrompt {
+    pub title: String,
+    pub content: String,
+    pub tags: Vec<String>,
+    pub categories: Vec<i64>,
+    pub excerpt: String,
+}
+
+// reads & returns content of files
 fn read_file(file_path:String) -> io::Result<String> {
     let content: String = fs::read_to_string(file_path)?;
-    println!("Got Email Contents");
     Ok(content)
 }
 
 /// prompts gpt with email content
-pub fn gpt_prompt() -> Result<String, Box<dyn std::error::Error>> {
+pub fn gpt_prompt() -> Result<GPTPrompt, Box<dyn std::error::Error>> {
     // set up variables & client
     dotenv().ok();
     let openai_api_key = env::var("OPENAI_API_KEY").unwrap();
     let client = Client::new(openai_api_key);
-
-    let email_content = read_file("email.txt").map_err(|_| "Failed to get email content")?;
-    let gpt_prompt = read_file("gpt_prompt.txt").map_err(|_| "Failed to get gpt prompt content")?;
+    
+    let email_content = read_file("./email.txt".to_string()).map_err(|_| "Failed to get email content")?;
+    let gpt_prompt = read_file("./gpt_prompt.txt".to_string()).map_err(|_| "Failed to get gpt prompt content")?;
 
     if email_content.trim().is_empty() {
         return Err("Email content is empty!".into());
@@ -52,13 +61,40 @@ pub fn gpt_prompt() -> Result<String, Box<dyn std::error::Error>> {
     );
 
     // send request
-    let result = client.chat_completion(req)?;
+    let result: chat_completion::ChatCompletionResponse = client.chat_completion(req)?;
     println!("{}", "Prompting Done!".blue());
 
+    // parse response
+    let gpt_result: String = result.choices.get(0).and_then(|choice| choice.message.content.clone()).unwrap_or("".to_string());
+    let gpt_body: serde_json::Value = serde_json::from_str(&gpt_result)?;
+
+    // save response to text file for debugging
+    let gpt_body_string = serde_json::to_string(&gpt_body).unwrap();
+    let mut file = File::create("./gpt_response.json")?;
+    file.write_all(&gpt_body_string.as_bytes())?;
+    println!("{}", "Saved reponse to JSON".bold());
+
+    // parse response into GPTPrompt struct
+    let tags: Result<Vec<String>, _> = gpt_body["tags"].as_array()
+        .ok_or("Expected an array")?
+        .iter()
+        .map(|v| v.as_str().ok_or("Expected a string").map(|s| s.to_owned()))
+        .collect();
+
+    let categories: Result<Vec<i64>, _> = gpt_body["categories"].as_array()
+        .ok_or("Expected an array")?
+        .iter()
+        .map(|v| v.as_i64().ok_or("Expected a string").map(|s| s.to_owned()))
+        .collect();
+
+    let response: GPTPrompt = GPTPrompt{
+        title: gpt_body["title"].to_string(),
+        content: gpt_body["content"].to_string(),
+        tags: tags?,
+        categories: categories?,
+        excerpt: gpt_body["excerpt"].to_string(),
+    };
     // return result
-    match result.choices.get(0).and_then(|choice| choice.message.content.clone()) {
-        Some(content) => Ok(content),
-        None => Err("No content available".into()),
-    }
+    Ok(response)
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
-// mod gpt_prompt;
-// use gpt_prompt::gpt_prompt;
-mod wordpress;
-use wordpress::wordpress;
+mod gpt_prompt;
+use gpt_prompt::gpt_prompt;
+use gpt_prompt::GPTPrompt;
+// mod wordpress;
+// use wordpress::find_tag;
 // use std::error::Error;
 
 fn main (){
-    wordpress();
-    // println!("{}", result.unwrap());
+    // create post text
+    let content_result = gpt_prompt();
+    let content: GPTPrompt = match content_result {
+        Ok(content) => content,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            return;
+        }
+    };
+
+    println!("{}", content.title);
+
+    // iteratively create & store tags to be used in post
+
+    // let tag_array: Vec<str> = vec![];
+
+    // let result = find_tag("kendrick lamar".to_string());
 }
+// async function convertTagsToIDs(post) {
+//   if (!post.tags || !Array.isArray(post.tags)) {
+//     throw new Error('No tags array found or the tags property is not an array.');
+//   }
+
+//   const tagPromises = post.tags.map(tagName => createTag(tagName));
+//   const tagIDs = await Promise.all(tagPromises);
+
+//   return tagIDs;
+// }
+// post.tags = await convertTagsToIDs(post);
