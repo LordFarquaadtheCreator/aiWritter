7e31240811e95aa2cf2ff3e59defb8ae0bf20677
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-01-05 14:46:23 -0500

    post to instagram works - just has minimal error handling

--- js/postToInstagram.mjs
+++ js/postToInstagram.mjs
@@ -5,34 +5,36 @@ async function createContainer(photoUrl, hashtags){
     const caption = encodeURIComponent(`Yeah we did that! Check this out and more with the link in bio!\n.\n.\n.\n${hashtags}`);
     const userId = process.env.GRAPH_USER_ID;
     const accessToken = process.env.GRAPH_ACCESS_TOKEN;
-
     let postId = '';
 
     const url = `https://graph.facebook.com/v18.0/${userId}/media?image_url=${photoUrl}&caption=${caption}&access_token=${accessToken}`;
-    axios.post(url)
-    .then((response) => {
-        response.status == 200 ? postId = response.data.id : console.log("Error: ", response);
-    })
-    .catch((error) =>{
-        console.log(error);
-    });
-    
+    try {
+        const response = await axios.post(url); // no error handling
+        // console.log(response);
+        postId = response.data.id;
+        console.log('Post ID:', postId);
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
     return postId;
 };
 async function publishContainer(postId){
     const userId = process.env.GRAPH_USER_ID;
     const accessToken = process.env.GRAPH_ACCESS_TOKEN;
-    const url = `https://graph.facebook.com/v18.0/${userId}/media_publish
-  ?creation_id=${postId}&access_token=${accessToken}`;
-    const success = false;
+    const url = `https://graph.facebook.com/v18.0/${userId}/media_publish?creation_id=${postId}&access_token=${accessToken}`;
+    let success = false;
 
-    axios.post(url)
-    .then((response) => {
-        response.status == 200 ? success = true : console.log("Error: ", response);
-    })
-    .catch((error) =>{
-        console.log(error);
-    });
+    try{
+        await axios.post(url)
+        .then((response) => {
+            success = true
+        })
+        .catch((error) =>{
+            console.log(error);
+        });
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
 
     return success;
 }
