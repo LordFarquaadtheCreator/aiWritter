3d68f2472c04d264859dc0c3f05755310576e895
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-01-05 21:41:27 -0500

    Add image manipulation and text creation functionality

--- js/imageMaker.mjs
+++ js/imageMaker.mjs
@@ -15,45 +15,62 @@ async function drawBackProduct(canvas, ctx, backProduct){
     // center image on canvas
     ctx.drawImage(backProduct, centerX, centerY, backProduct.width*scalar, backProduct.height*scalar);
 }
-
+/**
+ * creates a new canvas instance that has a smaller image in it
+ * @param {canvas} canvas 
+ * @param {?} ctx 
+ * @param {image} frontProduct 
+ */
 async function drawFrontProduct(canvas, ctx, frontProduct){
-    // new idea: i can just create a smaller canvas, scale the image to that, and render it's product 
-    // wont have to use a mask or anything
-    const canvas2 = createCanvas(1000, 1000);
-    const ctx2 = canvas.getContext('2d');
-
-
+    const canvas2 = createCanvas(800, 800);
+    const ctx2 = canvas2.getContext('2d');
 
     // get shorter side of image to determine the scaler
     let shorter = 0; let scalar = 0;
     frontProduct.naturalHeight > frontProduct.naturalWidth ? shorter = frontProduct.naturalWidth : shorter = backProduct.naturalHeight;
-    shorter < canvas.width ? scalar = shorter/(canvas.width - 200) : scalar = (canvas.width-200)/shorter;
+    shorter < canvas2.width ? scalar = shorter/canvas2.width : scalar = canvas2.width/shorter;
     // use same proportion to scale longer side
-    let centerX = (canvas.width - frontProduct.width * scalar) / 2;
-    let centerY = (canvas.height - frontProduct.height * scalar) / 2;
+    let centerX = (canvas2.width - frontProduct.width * scalar) / 2;
+    let centerY = (canvas2.height - frontProduct.height * scalar) / 2;
     
-    // Draw the image that you want to mask
-    ctx.drawImage(frontProduct, 
+    ctx2.drawImage(frontProduct, 
         centerX, centerY, 
         frontProduct.width*scalar, frontProduct.height*scalar
     );
+    centerX = (canvas.width - canvas2.width) / 2;
+    centerY = (canvas.height - canvas2.height) / 2;
 
-    let maskSize = canvas.width * 0.75; // Scale down the mask size
-    let maskX = (canvas.width - maskSize) / 2; // Center the mask
-    let maskY = (canvas.height - maskSize) / 2; // Center the mask
-
-    // Set the composite operation to 'destination-in' to create the mask effect
-    ctx.globalCompositeOperation = 'destination-in';
-
-    ctx.beginPath();
-    ctx.rect(maskX, maskY, maskSize, maskSize);
-    ctx.fill();
-
-    // Reset the composite operation to its default value
-    ctx.globalCompositeOperation = 'source-over';
+    ctx.drawImage(canvas2, centerX, centerY);
 }
+async function createText(text){
+    const textCanvas = createCanvas(800, 300);
+    const textCtx = textCanvas.getContext('2d');
 
-function drawTextBackground(ctx, x, y, width, height, radius) {
+    // Draw text
+    textCtx.fillStyle = 'white';
+    textCtx.font = '75px JollyRoger';
+    textCtx.fillText(text, 75, 100, 800);
+
+    const output = fs.createWriteStream('../righton-images/text.png');
+    const stream = textCanvas.createJPEGStream({ quality: 0.95 });
+    stream.pipe(output);
+    output.on('finish', () => console.log('Text created!'))
+}
+async function drawText(ctx, canvas, textImg){
+    ctx.drawImage(textImg, 0, canvas.height-400);
+
+}
+/**
+ * 
+ * @param {ctx} ctx 
+ * @param {int} x 
+ * @param {int} y 
+ * @param {int} width 
+ * @param {int} height 
+ * @param {int} radius 
+ * @param {Array} color 
+ */
+function drawTextBackground(ctx, x, y, width, height, radius, color) {
     ctx.beginPath();
     ctx.moveTo(x + radius, y);
     ctx.lineTo(x + width - radius, y);
@@ -64,15 +81,13 @@ function drawTextBackground(ctx, x, y, width, height, radius) {
     ctx.arcTo(x, y + height, x, y + height - radius, radius);
     ctx.lineTo(x, y + radius);
     ctx.arcTo(x, y, x + radius, y, radius);
-    ctx.fillStyle = 'rgba(0, 0, 255, 1';
+    ctx.fillStyle = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${color[3]})`;
     ctx.globalCompositeOperation = 'multiply';
     ctx.fill();
     ctx.globalCompositeOperation = 'source-over'; 
 }
 
-
 async function manipulateImages() {
-    // Create a canvas
     const canvas = createCanvas(1000, 1000);
     const ctx = canvas.getContext('2d');
 
@@ -82,19 +97,24 @@ async function manipulateImages() {
     const frontProduct = await loadImage('../righton-images/img.jpg');
 
     // Draw images on the canvas
-    // draw backProduct first
     await drawBackProduct(canvas, ctx, backProduct);
+
+    // get highlight color
+    let imageData = ctx.getImageData(200, 700, 1, 1);
+    let color = imageData.data; // An array [r, g, b, a]
+
+    // draw front product
     await drawFrontProduct(canvas, ctx, frontProduct);
+    // draw logo
+    ctx.drawImage(logo, (canvas.width-logo.width)-30, 30);
+    
+    // draw text
+    const text = 'sdfdsfsd\ndfsdsdfsdf\nweffdsaddfsd'
+    // drawTextBackground(ctx, -50, canvas.height-350, 600, 300, 50, color);
+    createText(text);
+    const textImg = await loadImage('../righton-images/text.png');
+    drawText(ctx, canvas, textImg)
 
-    ctx.drawImage(logo, (canvas.width-logo.width)-30, 30); // logo top right corner
-
-    // Draw text
-    ctx.fillStyle = 'white';
-    ctx.font = '30px Arial';
-    ctx.fillText('Hello, World!', 50, 50);
-
-    // draw text background
-    drawTextBackground(ctx, -50, canvas.height-350, 600, 300, 20);
 
     // Save the manipulated image to a file
     const output = fs.createWriteStream('output.jpg');
