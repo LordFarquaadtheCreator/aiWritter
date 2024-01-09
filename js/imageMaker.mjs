4ab2af68dd9414480ecf477a473765e854a61e28
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-01-08 23:45:08 -0500

    new work on imageMaker.js -> incomplete

--- js/imageMaker.mjs
+++ js/imageMaker.mjs
@@ -27,7 +27,7 @@ async function drawFrontProduct(canvas, ctx, frontProduct){
 
     // get shorter side of image to determine the scaler
     let shorter = 0; let scalar = 0;
-    frontProduct.naturalHeight > frontProduct.naturalWidth ? shorter = frontProduct.naturalWidth : shorter = backProduct.naturalHeight;
+    frontProduct.naturalHeight > frontProduct.naturalWidth ? shorter = frontProduct.naturalWidth : shorter = frontProduct.naturalHeight;
     shorter < canvas2.width ? scalar = shorter/canvas2.width : scalar = canvas2.width/shorter;
     // use same proportion to scale longer side
     let centerX = (canvas2.width - frontProduct.width * scalar) / 2;
@@ -42,6 +42,12 @@ async function drawFrontProduct(canvas, ctx, frontProduct){
 
     ctx.drawImage(canvas2, centerX, centerY);
 }
+/**
+ * creates an image with text on it in our desired font and size
+ * not dynamic, but good enough for now
+ * also needs to be able to handle long lines & break them up
+ * @param {string} text 
+ */
 async function createText(text){
     const textCanvas = createCanvas(800, 300);
     const textCtx = textCanvas.getContext('2d');
@@ -49,16 +55,17 @@ async function createText(text){
     // Draw text
     textCtx.fillStyle = 'white';
     textCtx.font = '75px JollyRoger';
-    textCtx.fillText(text, 75, 100, 800);
+    textCtx.textBaseline = 'bottom'; // anchor text to the bottom
+    textCtx.textAlign = 'left'; // anchor text to the left
+    textCtx.fillText(text,0,100);
 
-    const output = fs.createWriteStream('../righton-images/text.png');
-    const stream = textCanvas.createJPEGStream({ quality: 0.95 });
+    const output = fs.createWriteStream('./text.png');
+    output.on('error', console.error);
+    output.on('finish', () => console.log('Text created!'));
+
+    const stream = textCanvas.createPNGStream({ quality: 0.95 });
+    stream.on('error creating image', console.error);
     stream.pipe(output);
-    output.on('finish', () => console.log('Text created!'))
-}
-async function drawText(ctx, canvas, textImg){
-    ctx.drawImage(textImg, 0, canvas.height-400);
-
 }
 /**
  * 
@@ -86,15 +93,23 @@ function drawTextBackground(ctx, x, y, width, height, radius, color) {
     ctx.fill();
     ctx.globalCompositeOperation = 'source-over'; 
 }
-
-async function manipulateImages() {
+/**
+ * wht do u think
+ * @param {string} fileName 
+ */
+function readFile(fileName){
+    // const filePath = path.join(__dirname, fileName);
+    const data = fs.readFileSync(`./${fileName}`, 'utf-8');
+    return data;
+}
+async function createImage() {
     const canvas = createCanvas(1000, 1000);
     const ctx = canvas.getContext('2d');
 
     // Load images
     const logo = await loadImage('../righton-images/logo.png');
-    const backProduct = await loadImage('../righton-images/img.jpg');
-    const frontProduct = await loadImage('../righton-images/img.jpg');
+    const backProduct = await loadImage('../righton-images/img.png');
+    const frontProduct = await loadImage('../righton-images/img.png');
 
     // Draw images on the canvas
     await drawBackProduct(canvas, ctx, backProduct);
@@ -109,18 +124,20 @@ async function manipulateImages() {
     ctx.drawImage(logo, (canvas.width-logo.width)-30, 30);
     
     // draw text
-    const text = 'sdfdsfsd\ndfsdsdfsdf\nweffdsaddfsd'
-    // drawTextBackground(ctx, -50, canvas.height-350, 600, 300, 50, color);
-    createText(text);
-    const textImg = await loadImage('../righton-images/text.png');
-    drawText(ctx, canvas, textImg)
+    const text = await readFile("./title.txt");
+    await createText(text);
+
+    // draw text background & text on top
+    const textImg = await loadImage('./text.png');
+    drawTextBackground(ctx, -50, canvas.height-390, textImg.width, textImg.height, 50, color);
+    ctx.drawImage(textImg, 0, canvas.height-400);
 
 
     // Save the manipulated image to a file
-    const output = fs.createWriteStream('output.jpg');
-    const stream = canvas.createJPEGStream({ quality: 0.95 });
+    const output = fs.createWriteStream('output.png');
+    const stream = canvas.createPNGStream({ quality: 0.95 });
     stream.pipe(output);
     output.on('finish', () => console.log('Image created!'));
 }
 
-manipulateImages();
+createImage();
