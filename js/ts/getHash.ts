a8a988cc9551ae9f7e96ddea9073086c8cabf10d
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-14 18:38:29 -0400

    rewrote getHash in ts

---
+++ js/ts/getHash.ts
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+/**
+ * parses HTML to return hashtags, if any
+ * @param html string
+ * @returns Array<string> | null
+ */
+function parseHTML(html: string): Array<any> {
+  const $ = cheerio.load(html);  
+  const hashtags = $('div.tag-box').text();
+
+  let matches: RegExpMatchArray | null = hashtags.match(/#\w+/g);
+  
+  if (matches) {
+    matches = matches.slice(0, 30) as RegExpMatchArray | null;
+  }
+  return Array(matches);
+}
+
+/**
+ * @param {string} query 
+ * @returns array of hashtags, -1 if error
+ */
+export async function getHash(query: string): Promise<Array<string>> {
+  query = query.replace(/\s/g, '');
+  const options = {
+    method: 'GET',
+    url: `https://best-hashtags.com/hashtag/${query}/`,
+  };
+
+  try {
+    const response = await axios.request(options);
+    return(parseHTML(response.data));
+  } catch (error: any) {
+    throw new Error(`No response ${error.stack}`)
+  }
+}
+
+/**
+ * copies to clipboard
+ * @param data string
+ */
+function pbcopy(data: string) {
+  var proc = require('child_process').spawn('pbcopy'); 
+  proc.stdin.write(data); proc.stdin.end();
+}
+
+async function main(){
+  let query: string = process.argv[2] !== undefined ? process.argv[2] : process.exit(1);
+
+  let res: Array<string>;
+  try{
+    res = await getHash(query);
+  } catch (error: any) {
+    console.error(error.stack);
+    process.exit(1);
+  }
+
+  pbcopy(res.join(" "));
+}
+
+main()
