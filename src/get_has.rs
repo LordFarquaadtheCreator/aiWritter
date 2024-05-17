18567ce1bb71764ca530c3291012dbbcb0817758
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-05-17 09:58:47 -0400

    adding fn to get html in get_has

--- src/get_has.rs
+++ src/get_has.rs
@@ -1,8 +1,17 @@
 use cli_clipboard::{ClipboardContext, ClipboardProvider};
+use reqwest;
 
 fn copy_to_clipboard(text: &str) {
     let mut ctx = ClipboardContext::new().unwrap();
     ctx.set_contents(text.to_owned()).unwrap();
+}
+
+fn get_html(topic: &str) -> Result<String, reqwest::Error>{
+    let topic: String = topic.trim().replace(" ", "");
+    let url: String = format!("https://best-hashtags.com/hashtag/{}/", topic);
+    let body: String = reqwest::blocking::get(url)?.text()?;
+    
+    Ok(body)
 }
 
 fn main (){
