a03508a0a6167ce2d7593cdb01bd59c7cceb6db8
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-06-03 08:31:32 -0400

    updating index.mjs to use formdata

--- js/index.mjs
+++ js/index.mjs
@@ -1,20 +1,21 @@
 import express from 'express';
 import { handler } from './createPost.mjs';
-
+import multer from 'multer';
+const upload = multer();
 const app = express();
 
 app.get("/", (req, res) => {
     return res.send("whats up rocky").status(200)
 })
 
-app.post("/lambda", (req, res) => {
+app.post("/lambda", upload.none(), (req, res) => {
     const body = req.body;
 
-    if (!body || !body.email) return res.send("missing body or email")
+    if (!body || !body.email) return res.send(`missing body or email, got ${body}`)
 
     try{
         const response = handler(body);
-
+// 
         return response.send(response).status(200);
     } catch (error) {
         return response.send(Error(error.toString()));
@@ -23,4 +24,4 @@ app.post("/lambda", (req, res) => {
 
 app.listen(2222, () => {
     console.log("Server listening on ", 2222)
-})
+});
