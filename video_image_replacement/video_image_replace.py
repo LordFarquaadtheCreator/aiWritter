5ae02e03391e1e0b7deb83980dcfc7a39e796982
LordFarquaadtheCreator <fahadfaruqi1@gmail.com>
2024-07-16 11:29:10 -0400

    adding final touches to video_image_replace, i can't see errors so ill have to debug on the train

--- video_image_replacement/video_image_replace.py
+++ video_image_replacement/video_image_replace.py
@@ -5,15 +5,11 @@ import imagehash
 from moviepy.editor import VideoFileClip, AudioFileClip
 from save_video import save_video
 
-IMAGE_TO_DELETE = "frame_to_delete.png"
 REPLACEMENT_IMAGE = "target.png"
-INPUT_VIDEO_PATH = "../Downloads/GMT20240614-215723_Recording_640x360.mp4"
-OUTPUT_VIDEO_PATH_VIDEO = "edited_video.mp4"
-OUTPUT_VIDEO_PATH_FINAL = "processed_final.mp4"
 
-def process_video(target_hash):
+def process_video(target_hash, replacement_image, input_video_path):
     # Open the video file
-    cap = cv2.VideoCapture(INPUT_VIDEO_PATH)
+    cap = cv2.VideoCapture(input_video_path)
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     fps = cap.get(cv2.CAP_PROP_FPS)
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -21,7 +17,7 @@ def process_video(target_hash):
 
     out = cv2.VideoWriter(OUTPUT_VIDEO_PATH_VIDEO, fourcc, fps, (width, height))
 
-    print("Starting To Replace!")
+    print("Parsing Over Video")
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -44,13 +40,9 @@ def process_video(target_hash):
     cv2.destroyAllWindows()
 
 
-if __name__ == "__main__":
-    target_image = cv2.imread(IMAGE_TO_DELETE)
+def main(target_image_hashed, INPUT_VIDEO_PATH, OUTPUT_VIDEO_PATH_VIDEO, OUTPUT_VIDEO_PATH_FINAL):
     replacement_image = cv2.imread(REPLACEMENT_IMAGE)
 
-    target_image_pil = Image.open(IMAGE_TO_DELETE)
-    target_hash = imagehash.phash(target_image_pil); del(target_image_pil)
-
-    process_video(target_hash)
+    process_video(target_image_hashed, replacement_image, INPUT_VIDEO_PATH)
     out_path = save_video(INPUT_VIDEO_PATH, OUTPUT_VIDEO_PATH_VIDEO, OUTPUT_VIDEO_PATH_FINAL)
     print("Processing complete! Final video saved as", out_path)
