9dcc9f7a60db38ff2914a99081721f162fc53627
LordFarquaadtheCreator <fahadfaruqi1@gmail.com>
2024-07-16 10:34:19 -0400

    adding shell scripts to change profiles between work

---
+++ changeGitConfig.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git config --global user.name "LordFarquaadtheCreator"
+git config  --global user.email "fahadfaruqi1@gmail.com"
+echo "Set git config email to personal"
---
+++ resetGitConfig.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git config --global user.name "Fahad"
+git config  --global user.email "FFaruqi@dot.nyc.gov"
+echo "Set git config email to work"
