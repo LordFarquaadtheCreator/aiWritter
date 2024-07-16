205d0ba64a60969a3d0554753ab3afb5f8b0c4a1
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-07-16 19:59:42 -0400

    renamed file

--- video_image_replacement/main_image_replace.py
+++ video_image_replacement/main_image_replace.py
@@ -1,5 +1,7 @@
 def main_process():
-    INPUT_VIDEO_PATH = input("Enter relative file path of video file:\n")
+    INPUT_VIDEO_PATH = "/Users/fahadfaruqi/Downloads/" + input(
+        "Enter file name of video in Downloads folder:\n"
+    )
     if not os.path.exists(INPUT_VIDEO_PATH):
         print("Error: File not found")
         return main_process()
@@ -9,19 +11,24 @@ def main_process():
         print("Error: Invalid time")
         return main_process()
 
-    OUTPUT_VIDEO_PATH_VIDEO = "edited_video.mp4"  # hardcoded
-    OUTPUT_VIDEO_PATH_FINAL = "processed_final.mp4"  # hardcoded
-    IMAGE_TO_DELETE = get_frame_from_video(INPUT_VIDEO_PATH, int(FRAME_TO_GRAB))
+    OUTPUT_VIDEO_PATH_VIDEO = "assets/edited_video.mp4"  # hardcoded
+    OUTPUT_VIDEO_PATH_FINAL = "assets/processed_final.mp4"  # hardcoded
+    SHAPE, IMAGE_TO_DELETE = get_frame_from_video(INPUT_VIDEO_PATH, int(FRAME_TO_GRAB))
 
     USE_CYNTHIA = True
     if USE_CYNTHIA:
-        REPLACEMENT_IMAGE = "assets/cynthia.jpeg"  # hardcode if USE_CYNTHIA, future use only
+        REPLACEMENT_IMAGE = (
+            "assets/cynthia.jpeg"  # hardcode if USE_CYNTHIA, future use only
+        )
         print("The Default Photo of Cynthia will be used - target.png")
 
-    print("Please Wait as We Process the Video")
+    print("Generating Target Frame")
+    REPLACEMENT_FRAME = replacement_frame(SHAPE)
 
+    print("Please Wait as We Process the Video")
     main(
         IMAGE_TO_DELETE,
+        REPLACEMENT_FRAME,
         INPUT_VIDEO_PATH,
         OUTPUT_VIDEO_PATH_VIDEO,
         OUTPUT_VIDEO_PATH_FINAL,
@@ -32,5 +39,6 @@ if __name__ == "__main__":
     from video_image_replace import main
     from get_first_frame import get_frame_from_video
     import os
+    from generate_replacement_frame import replacement_frame
 
     main_process()
