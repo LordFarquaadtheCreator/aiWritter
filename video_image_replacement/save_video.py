b066789c2e01b76f68decae29d08dbd40421da04
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-07-17 08:56:33 -0400

    removing try catch cause it did nothing, letting error throw naturally

--- video_image_replacement/save_video.py
+++ video_image_replacement/save_video.py
@@ -1,19 +1,13 @@
 def save_video(old_video_path: str, new_video_path: str, output_file_name: str) -> str:
     from moviepy.editor import VideoFileClip
 
-    try:
-        old_video = VideoFileClip(old_video_path)
-        new_video = VideoFileClip(new_video_path)
-        # Extract audio from old video
-        audio = old_video.audio
-        # Combine new video with audio from old video
-        final_video = new_video.set_audio(audio)
-        # Write the final video (with audio) to disk
-        final_video.write_videofile(output_file_name, audio_codec="aac")
+    old_video = VideoFileClip(old_video_path)
+    new_video = VideoFileClip(new_video_path)
+    # Extract audio from old video
+    audio = old_video.audio
+    # Combine new video with audio from old video
+    final_video = new_video.set_audio(audio)
+    # Write the final video (with audio) to disk
+    final_video.write_videofile(output_file_name, audio_codec="aac")
 
-        return output_file_name
-    except Exception as ex:
-        print("Error occurred saving video and audio")
-        print(ex)
-        print("Exiting ...")
-        exit()
+    return output_file_name
