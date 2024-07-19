2a2aec1d500cd470e5bdc24c3b597233a8d79dd8
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-07-19 13:34:01 -0400

    renamed file & used os.getcwd to get root dir instead of harcoded

---
+++ video_image_replacement/aux_main.py
@@ -0,0 +1,45 @@
+def main_process():
+    from video_image_replace import video_image_replace
+    from get_first_frame import get_frame_from_video
+    import os
+    from generate_replacement_frame import replacement_frame
+
+    INPUT_VIDEO_PATH = "/Users/fahadfaruqi/Downloads/" + input(
+        "Enter file name of video in Downloads folder:\n"
+    )
+    if not os.path.exists(INPUT_VIDEO_PATH):
+        print("Error: File not found")
+        return main_process()
+
+    FRAME_TO_GRAB = input("Enter time of frame to replace from video (in seconds):\n")
+    if not FRAME_TO_GRAB.isdigit():
+        print("Error: Invalid time")
+        return main_process()
+
+    OUTPUT_VIDEO_PATH_VIDEO = "assets/edited_video.mp4"  # hardcoded
+    OUTPUT_VIDEO_PATH_FINAL = os.path.join(os.getcwd(), "Downloads/processed_final.mp4")
+    SHAPE, IMAGE_TO_DELETE = get_frame_from_video(INPUT_VIDEO_PATH, int(FRAME_TO_GRAB))
+
+    USE_CYNTHIA = True
+    if USE_CYNTHIA:
+        REPLACEMENT_IMAGE = (
+            "assets/cynthia.jpeg"  # hardcode if USE_CYNTHIA, future use only
+        )
+
+        print("The Default Photo of Cynthia will be used - target.png")
+
+    print("Generating Target Frame")
+    REPLACEMENT_FRAME = replacement_frame(SHAPE)
+
+    print("Please Wait as We Process the Video")
+    video_image_replace(
+        IMAGE_TO_DELETE,
+        REPLACEMENT_FRAME,
+        INPUT_VIDEO_PATH,
+        OUTPUT_VIDEO_PATH_VIDEO,
+        OUTPUT_VIDEO_PATH_FINAL,
+    )
+
+
+if __name__ == "__main__":
+    main_process()
