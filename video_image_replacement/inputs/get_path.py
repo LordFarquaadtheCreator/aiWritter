a2567276160835e86dfb8a5ede6fd8c461adaf15
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-07-22 14:06:03 -0400

    abstracted get input fns to files

---
+++ video_image_replacement/inputs/get_path.py
@@ -0,0 +1,19 @@
+def get_video_path(ROOT, EXIT_KEY) -> str:
+    """
+    gets video path from user
+    checks for invalid path
+    will recursively ask till exit key
+    """
+    import os
+    from exception import ExitQuery
+
+    input_video_path_temp = input("Enter file name of video in Downloads folder:\n")
+    INPUT_VIDEO_PATH = os.path.join(ROOT, "Downloads", input_video_path_temp)
+
+    if input_video_path_temp == EXIT_KEY:
+        raise ExitQuery
+    if not os.path.exists(INPUT_VIDEO_PATH):
+        print("Error: File not found", INPUT_VIDEO_PATH)
+        return get_video_path(ROOT, EXIT_KEY)
+
+    return INPUT_VIDEO_PATH
---
+++ video_image_replacement/inputs/get_time.py
@@ -0,0 +1,16 @@
+def get_time_stamp(EXIT_KEY) -> int:
+    """
+    gets time stamp of frame to grab from user
+    checks for floats and strings
+    recursively asks till exit key is pressed
+    """
+    from exception import ExitQuery
+
+    FRAME_TO_GRAB = input("Enter time of frame to replace from video (sec):\n")
+    if FRAME_TO_GRAB == EXIT_KEY:
+        raise ExitQuery
+    if not FRAME_TO_GRAB.isdigit():
+        print("Error: input must be an integer")
+        return get_time_stamp(EXIT_KEY)
+
+    return int(FRAME_TO_GRAB)
