a170cc284e95ae4e72cebb89e8816a8c659a5cad
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-07-22 14:00:20 -0400

    abstracted user inputs into fns to exit on exit key

--- video_image_replacement/aux_main.py
+++ video_image_replacement/aux_main.py
@@ -1,22 +1,45 @@
+def get_video_path(ROOT, EXIT_KEY) -> str:
+    import os
+    from exception import ExitQuery
+
+    input_video_path_temp = input("Enter file name of video in Downloads folder:\n")
+    INPUT_VIDEO_PATH = os.path.join(ROOT, "Downloads", input_video_path_temp)
+    if input_video_path_temp == EXIT_KEY:
+        raise ExitQuery
+    if not os.path.exists(INPUT_VIDEO_PATH):
+        print("Error: File not found", INPUT_VIDEO_PATH)
+        return get_video_path(ROOT, EXIT_KEY)
+
+    return INPUT_VIDEO_PATH
+
+
+def get_time_stamp(EXIT_KEY) -> int:
+    from exception import ExitQuery
+
+    FRAME_TO_GRAB = input("Enter time of frame to replace from video (sec):\n")
+    if FRAME_TO_GRAB == EXIT_KEY:
+        raise ExitQuery
+    if not FRAME_TO_GRAB.isdigit():
+        print("Error: input must be an integer")
+        return get_time_stamp(EXIT_KEY)
+
+
 def main_process():
     from video_image_replace import video_image_replace
     from get_first_frame import get_frame_from_video
     import os
     from generate_replacement_frame import replacement_frame
-    # GMT20240614-215723_Recording_640x360.mp4
-    PWD = os.path.dirname(os.path.realpath(__file__))
-    ROOT = '/Users/fahadfaruqi'
-    INPUT_VIDEO_PATH = os.path.join(ROOT, "Downloads", input(
-        "Enter file name of video in Downloads folder:\n"
-    ))
-    if not os.path.exists(INPUT_VIDEO_PATH):
-        print("Error: File not found", INPUT_VIDEO_PATH)
-        return main_process()
+    from exception import ExitQuery
 
-    FRAME_TO_GRAB = input("Enter time of frame to replace from video (in seconds):\n")
-    if not FRAME_TO_GRAB.isdigit():
-        print("Error: Invalid time")
-        return main_process()
+    EXIT_KEY = "q"
+    ROOT = "/Users/fahadfaruqi"
+    PWD = os.path.dirname(os.path.realpath(__file__))
+
+    try:
+        INPUT_VIDEO_PATH = get_video_path(ROOT, EXIT_KEY)
+        FRAME_TO_GRAB = get_time_stamp(EXIT_KEY)
+    except ExitQuery:
+        return
 
     OUTPUT_VIDEO_PATH_VIDEO = os.path.join(PWD, "assets/edited_video.mp4")  # hardcoded
     OUTPUT_VIDEO_PATH_FINAL = os.path.join(ROOT, "Downloads/processed_final.mp4")
@@ -24,9 +47,9 @@ def main_process():
 
     USE_CYNTHIA = True
     if USE_CYNTHIA:
-        REPLACEMENT_IMAGE = (
-            os.path.join(PWD, "assets/cynthia.jpeg")  # hardcode if USE_CYNTHIA, future use only
-        )
+        REPLACEMENT_IMAGE = os.path.join(
+            PWD, "assets/cynthia.jpeg"
+        )  # hardcode if USE_CYNTHIA, future use only
 
         print("The Default Photo of Cynthia will be used - target.png")
 
