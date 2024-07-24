31330a81c55aaaeedbba6718aa5272ae4ce9e25d
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-07-24 19:48:48 -0400

    adding more test

--- video_image_replacement/tests.py
+++ video_image_replacement/tests.py
@@ -1,14 +1,12 @@
 class unit_tests:
-    def __init__(self):
+    def __mute__(self):
         import io
         import sys
 
-        # Mute Outputs
         text_trap = io.StringIO()
         sys.stdout = text_trap
 
     def __cleanup__(self):
-        import io
         import sys
 
         sys.stdout = sys.__stdout__
@@ -16,21 +14,33 @@ class unit_tests:
     def test_get_frame(self):
         from utils.get_first_frame import get_frame_from_video
 
+        self.__mute__()
+
         _, _ = get_frame_from_video("mocks/video.mp4", 0)
         _, _ = get_frame_from_video("mocks/video.mp4", -1)
 
         self.__cleanup__()
 
+    def test_generate_replacement_frame(self):
+        from utils.generate_replacement_frame import replacement_frame
+        import os
+
+        self.__mute__()
+
+        _ = replacement_frame(os.getcwd(), (100, 100))
+
+        self.__cleanup__()
+
 
 if __name__ == "__main__":
+    """Shallow Tests, assumes modules it tests has proper error handling"""
     myTest = unit_tests()
     num_pass = 0
     num_tests = len(
         [
             method
             for method in dir(unit_tests)
-            if callable(getattr(unit_tests, method)) 
-            and not method.startswith("__")
+            if callable(getattr(unit_tests, method)) and not method.startswith("__")
         ]
     )
 
@@ -40,5 +50,13 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"Get Frame Test Failed: {e}")
 
+    try:
+        myTest.test_generate_replacement_frame()
+        num_pass += 1
+    except Exception as e:
+        print(f"Generate Frame Test Failed: {e}")
 
     print(f"Passed {num_pass}/{num_tests}")
+
+    if num_pass != num_tests:
+        exit(1)
