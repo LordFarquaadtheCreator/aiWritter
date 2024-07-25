cdb68e5e1eba483ab47ea05510889292b4254939
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-07-24 20:37:08 -0400

    changed cli interface file name

--- video_image_replacement/cli.py
+++ video_image_replacement/cli.py
@@ -2,9 +2,11 @@ def main():
     import subprocess
     import os
 
-    print("Hit 'q' to exit the application")
     DIR_PATH = os.path.dirname(os.path.realpath(__file__))
     AUX_MAIN_PATH = os.path.join(DIR_PATH, "aux_main.py")
+
+    print("Hit 'q' to exit the application")
+
     venv_path = os.path.join(DIR_PATH, "env")
     if not os.path.exists(venv_path):
         raise FileNotFoundError("Virtual environment not found at {}".format(venv_path))
