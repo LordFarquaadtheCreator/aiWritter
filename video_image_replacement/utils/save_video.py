465983ece2ec4fa7005eeb80eac05fb435b8821e
Fahad Faruqi <fahadfaruqi1@gmail.com>
2025-02-22 13:10:38 -0500

    video image replace checks to see if previous video exists before removing it to write new video

--- video_image_replacement/utils/save_video.py
+++ video_image_replacement/utils/save_video.py
@@ -1,5 +1,6 @@
 def save_video(old_video_path: str, new_video_path: str, output_file_name: str) -> str:
     from moviepy.editor import VideoFileClip
+    import os
 
     old_video = VideoFileClip(old_video_path)
     new_video = VideoFileClip(new_video_path)
@@ -8,6 +9,10 @@ def save_video(old_video_path: str, new_video_path: str, output_file_name: str) 
     # Combine new video with audio from old video
     final_video = new_video.set_audio(audio)
     # Write the final video (with audio) to disk
+    
+    if os.access(output_file_name, os.F_OK):
+        os.remove(output_file_name)
+    
     final_video.write_videofile(
         output_file_name, audio_codec="aac", verbose=False, logger=None
     )
